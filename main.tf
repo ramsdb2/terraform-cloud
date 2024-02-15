0f5f3387446b04ae632562b272daf3fac895d225
ramsdb2 <156233195+ramsdb2@users.noreply.github.com>
2024-02-15 11:58:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ provider "null" {
 }
 
 /* a list of local variables */
+/* a list of local variables */
 locals {
-  jones = ["indy", "henry", "marian"]
+  jones = ["indy", "henry", "marian", "katanga"]
 }
-
 
 /* The null_resource implements the standard resource lifecycle but takes no more action */
 resource "null_resource" "jones" {
